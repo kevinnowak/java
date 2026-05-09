2ed38e03ce20cb7dd592a3102e5685934c27b975
Kevin <kevin@mb-air-1.home>
2026-05-09 17:15:05 +0200

    Update

---
+++ collections/src/main/java/com/github/kevinnowak/java/collections/MyIterable.java
@@ -0,0 +1,11 @@
+package com.github.kevinnowak.java.collections;
+
+import java.util.Iterator;
+
+public class MyIterable<T> implements Iterable<T> {
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
