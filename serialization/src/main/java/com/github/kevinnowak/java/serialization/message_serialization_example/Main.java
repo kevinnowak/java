d8ef39553f3c16bf61eb0824fa55fe630d0e8bd0
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2026-05-15 23:41:57 +0200

    Update

--- serialization/src/main/java/com/github/kevinnowak/java/serialization/message_serialization_example/Main.java
+++ serialization/src/main/java/com/github/kevinnowak/java/serialization/message_serialization_example/Main.java
@@ -2,47 +2,44 @@ package com.github.kevinnowak.java.serialization.message_serialization_example;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
 public class Main {
 
     static void main() {
-        Message message = new Message(
-                UUID.randomUUID(),
-                Instant.now(),
-                "Bob",
-                "Hello, world!",
-                10
-        );
 
-        IO.println(message);
-
+        var messages = generateMessages(1_000);
         MessageCodec nativeCodec = new NativeCodec();
-        var nativeBytes = nativeCodec.encode(message);
-
         MessageCodec jsonCodec = new JsonCodec();
-        var jsonBytes = jsonCodec.encode(message);
+        List<MessageCodec> codecs = List.of(nativeCodec, jsonCodec);
 
-        IO.println(nativeCodec.name() + " length: " + nativeBytes.length);
-        IO.println(jsonCodec.name() + " length: " + jsonBytes.length);
+
+        codecs.forEach(messageCodec -> {
+            long start = System.nanoTime();
+            var blobs = new ArrayList<byte[]>();
+            messages.forEach(message -> blobs.add(messageCodec.encode(message)));
+
+            long totalBytes = blobs.stream()
+                    .mapToInt(blob -> blob.length)
+                    .sum();
+
+            long end = System.nanoTime();
+            IO.println(messageCodec.name() + ": " + (end - start) / 1_000_000);
+            IO.println("Total bytes: " + totalBytes);
+        });
     }
 
     static List<Message> generateMessages(int count) {
-        List<Message> messages = new ArrayList<>();
-
-        for (int i = 0; i < count; i++) {
-            Message message = new Message(
+        return IntStream.range(0, count)
+                .mapToObj(_ -> new Message(
                     UUID.randomUUID(),
                     Instant.now(),
                     "Bob",
                     "Hello, world!",
-                    10
-            );
-
-            messages.add(message);
-        }
-
-        return messages;
+                    10))
+                .toList();
     }
 }
