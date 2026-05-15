f5b9a9aa69726f3034049b4ac1b1e5e7a7e95797
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2026-05-15 23:48:16 +0200

    Update

---
+++ lambdas/src/main/java/com/github/kevinnowak/java/lambdas/StringSorter.java
@@ -0,0 +1,59 @@
+package com.github.kevinnowak.java.lambdas;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringSorter {
+    private List<String> strings;
+
+    public List<String> getStrings() {
+        return strings;
+    }
+
+    public void setStrings(List<String> strings) {
+        this.strings = strings;
+    }
+
+    public List<String> naturalSort() {
+        Collections.sort(strings);
+
+        return strings;
+    }
+
+    public List<String> naturalSortWithStreams() {
+        return strings.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public List<String> lengthSortWithStreams() {
+        return strings.stream()
+                .sorted((s1, s2) -> Integer.compare(s1.length(), s2.length()))
+                .collect(Collectors.toList());
+    }
+
+    public List<String> lengthSortWithComparator() {
+        Collections.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.length() - s2.length();
+            }
+        });
+
+        return strings;
+    }
+
+    public List<String> lengthSortWithLambda() {
+        Collections.sort(strings, (String s1, String s2) -> s1.length() - s2.length());
+
+        return strings;
+    }
+
+    public List<String> lengthSortWithStaticMethodReference() {
+        Collections.sort(strings, Comparator.comparingInt(String::length));
+
+        return strings;
+    }
+}
